dee2353e4e6ff7c1f2e97799360cb01337c073a4
sid <68276495+siddant-rachha@users.noreply.github.com>
2023-04-17 02:52:59 +0530

    changed card width and look

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -24,17 +24,17 @@ const CARD = ({ name, email, dob, country, avatar, id, handleEdit, handleDelete 
 
   return (
     <>
-      <Card className={`${classes.card}`+" shadow"}>
+      <Card className={`${classes.card}` + " shadow"}>
         <Card.Body className={classes.cardBody}>
           <Image src={avatar} className={classes.cardImg} roundedCircle />
-          <Card.Title className={classes.cardTitle}>{name}</Card.Title>
           <Card.Body className={classes.icons}>
             <Card.Link as={Link} to="/update" onClick={(e) => handleEditClick(e)}><FontAwesomeIcon className={classes.editIcon} icon={faEdit} /></Card.Link>
-            <Card.Link as={Link} onClick={(e) => handleDeleteClick(e)} ><FontAwesomeIcon className={`${classes.deleteIcon}`+" text-danger"} icon={faTrash} /></Card.Link>
+            <Card.Link as={Link} onClick={(e) => handleDeleteClick(e)} ><FontAwesomeIcon className={`${classes.deleteIcon}` + " text-danger"} icon={faTrash} /></Card.Link>
           </Card.Body>
 
         </Card.Body>
         <ListGroup className="list-group-flush">
+          <ListGroup.Item>Name: {name}</ListGroup.Item>
           <ListGroup.Item>Email: {email}</ListGroup.Item>
           <ListGroup.Item>Dob: {dob}</ListGroup.Item>
           <ListGroup.Item>Country: {country}</ListGroup.Item>
